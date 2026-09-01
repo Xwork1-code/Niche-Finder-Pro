9093a37131ae90d43eccee0219bf67966228e8f1
Xwork1-code <pocketmario80@gmail.com>
2026-09-01 21:55:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { Pool } = require("pg");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
